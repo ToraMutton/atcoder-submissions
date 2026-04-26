b00b9e4a09a9997dc0222036bc6078f3a3624400
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-04-26 23:13:55 +0900

    feat: implement solution for ABC081B

--- abs/src/bin/abc081b.rs
+++ abs/src/bin/abc081b.rs
@@ -1,3 +1,22 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        a: [i64; n],
+    }
+    
+    let mut ans = i64::MAX;
+
+    for &x in &a {
+        let mut count = 0;
+        let mut num = x;
+        while num % 2 == 0 {
+            num /= 2;
+            count += 1;
+        }
+        ans = ans.min(count)
+    }
+
+    println!("{}", ans);
 }
