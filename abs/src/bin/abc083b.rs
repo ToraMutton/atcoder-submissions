01af6236f3cb09fd5ec6f28a86cb9b742e2ce192
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-04-27 17:37:55 +0900

    feat: implement solution for ABC083B

--- abs/src/bin/abc083b.rs
+++ abs/src/bin/abc083b.rs
@@ -1,3 +1,25 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        a: i64,
+        b: i64,
+    }
+
+    let mut all = 0;
+
+    for i in 1..=n {
+        let mut sum = 0;
+        let mut temp = i;
+        while temp > 0 {
+            sum += temp % 10;
+            temp /= 10;
+        }
+        if a <= sum && sum <= b {
+            all += i;
+        }
+    }
+
+    println!("{}", all);
 }
