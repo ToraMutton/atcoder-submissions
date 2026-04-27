afdeb1deaf6014db72b321708a42d0088da5b256
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-04-27 12:58:42 +0900

    feat: implement solution for ABC087B

--- abs/src/bin/abc087b.rs
+++ abs/src/bin/abc087b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        a: i64,
+        b: i64,
+        c: i64,
+        x: i64,
+    }
+
+    let mut count = 0;
+
+    for i in 0..=a {
+        for j in 0..=b {
+            let rest = x -i*500 - j*100;
+            if rest >= 0 && rest % 50 == 0 && rest / 50 <= c {
+                count += 1;
+            }
+        }
+    }
+
+    println!("{}", count);
 }
