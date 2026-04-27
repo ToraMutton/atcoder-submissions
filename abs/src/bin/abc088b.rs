e43d6c2fc2b26716684336d8194796e62c4b8b58
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-04-27 20:51:27 +0900

    feat: implement solution for ABC088B

--- abs/src/bin/abc088b.rs
+++ abs/src/bin/abc088b.rs
@@ -1,3 +1,23 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: usize,
+        mut a: [i64; n],
+    }
+
+    let mut sum_a = 0;
+    let mut sum_b = 0;
+
+    a.sort();
+    a.reverse();
+
+    for i in 0..n as usize {
+        if i % 2 == 0 {
+            sum_a += a[i];
+        } else {
+            sum_b += a[i];
+        }
+    }
+    println!("{}", sum_a - sum_b);
 }
