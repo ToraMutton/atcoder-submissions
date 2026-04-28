8ec38a78a349d0300200d8ef9faa76f414d76ec3
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-04-28 23:48:31 +0900

    feat: implement solution for ABC085B

--- abs/src/bin/abc085b.rs
+++ abs/src/bin/abc085b.rs
@@ -1,5 +1,23 @@
+use proconio::input;
+
 fn main() {
     input! {
-        
+        n: usize,
+        mut a: [i64; n],
     }
+
+    let mut count = 1;
+
+    a.sort();
+    a.reverse();
+
+    let mut max = a[0];
+
+    for i in 1..n as usize {
+        if max > a[i] {
+            max = a[i];
+            count += 1;
+        }
+    }
+    println!("{}", count);
 }
