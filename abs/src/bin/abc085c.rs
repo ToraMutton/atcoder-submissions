8de8856f40f1743ea7dcf8733f4f18ab412e1d3e
ToraMutton <205386536+ToraMutton@users.noreply.github.com>
2026-04-29 23:24:06 +0900

    feat: implement solution for ABC085C

--- abs/src/bin/abc085c.rs
+++ abs/src/bin/abc085c.rs
@@ -1,3 +1,20 @@
+use proconio::input;
+
 fn main() {
-    todo!();
+    input! {
+        n: i64,
+        y: i64,
+    }
+
+    for i in 0..=n {
+        for j in 0..=n {
+            let el = y - 10000*i - 5000*j;
+            let k = el / 1000;
+            if el >= 0 && k >= 0 && el % 1000 == 0 && (i + j + k) == n {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
+    }
+    println!("-1 -1 -1");
 }
